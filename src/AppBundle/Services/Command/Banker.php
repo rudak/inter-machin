2fe5ad9d81493f9d3e8227ef762ba97828522693
Arnaud Kadur <akadur@nvl.fr>
2018-04-04 14:29:23 +0200

    small refacto

--- src/AppBundle/Services/Command/Banker.php
+++ src/AppBundle/Services/Command/Banker.php
@@ -27,19 +27,11 @@ class Banker extends CronEmCommand
             $loan->setStatus(Loan::STATUS_VALID);
             $loan->setExpiration(new \DateTime('+5 days'));
             $loan->getUser()->addMoney($loan->getAmount());
+            $message = sprintf("Votre pret de %d$ a été accepté. Il devra être remboursé avant le %s. Merci a vous.", $loan->getAmount(), $loan->getDate()->format('d/m/Y'));
             $this->em->persist(
-                NotificationCreator::getNotification(
-                    $loan->getUser(),
-                    sprintf("Votre pret de %d$ a été accepté. Il devra être remboursé avant le %s. Merci a vous.", $loan->getAmount(), $loan->getDate()->format('d/m/Y')),
-                    Notification::TYPE_LOAN_VALIDATION)
-            );
+                NotificationCreator::getNotification($loan->getUser(), $message, Notification::TYPE_LOAN_VALIDATION));
             $this->em->persist(
-                LogCreator::getLog(
-                    $loan->getUser(),
-                    false,
-                    sprintf("Votre pret de %d$ a été accepté. Il devra être remboursé avant le %s. Merci a vous.", $loan->getAmount(), $loan->getDate()->format('d/m/Y')),
-                    LogCreator::TYPE_BANKER)
-            );
+                LogCreator::getLog($loan->getUser(), false, $message, LogCreator::TYPE_BANKER));
             $this->em->persist($loan);
         }
         $this->em->flush();
