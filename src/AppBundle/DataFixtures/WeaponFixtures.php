3a1efe001fc38c9f40af0601b308df53018b8e67
rudak <roiduboul@gmail.com>
2018-04-09 20:09:19 +0200

    refacto fixtures imgs weapon

--- src/AppBundle/DataFixtures/WeaponFixtures.php
+++ src/AppBundle/DataFixtures/WeaponFixtures.php
@@ -11,31 +11,38 @@ use Symfony\Component\Filesystem\Filesystem;
 class WeaponFixtures extends Fixture
 {
 
-    const IMG_PATH = 'img_name';
+    const WEAPON_NAME    = 'weapon-name';
+    const WEAPON_ATTACK  = 'weapon-attack';
+    const WEAPON_DEFENSE = 'weapon-defense';
+    const WEAPON_PRICE   = 'weapon-price';
+    const WEAPON_USES    = 'weapon-uses';
+    const WEAPON_LVL     = 'weapon-lvl';
+    const IMG_PATH       = 'img-path';
+    const IMG_NAME       = 'img-name';
 
     public function load(ObjectManager $manager)
     {
         foreach ($this->getWeaponsData() as $weaponData) {
             $weapon = new Weapon();
-            $weapon->setName($weaponData['name']);
-            $weapon->setAttack($weaponData['attack']);
-            $weapon->setDefense($weaponData['defense']);
-            $weapon->setPrice($weaponData['price']);
-            $weapon->setUses($weaponData['uses']);
-            $weapon->setLvl($weaponData['lvl']);
+            $weapon->setName($weaponData[self::WEAPON_NAME]);
+            $weapon->setAttack($weaponData[self::WEAPON_ATTACK]);
+            $weapon->setDefense($weaponData[self::WEAPON_DEFENSE]);
+            $weapon->setPrice($weaponData[self::WEAPON_PRICE]);
+            $weapon->setUses($weaponData[self::WEAPON_USES]);
+            $weapon->setLvl($weaponData[self::WEAPON_LVL]);
             $weapon->setImage(
-                $this->getImage($weaponData[self::IMG_PATH])
+                $this->getImage($weaponData)
             );
             $manager->persist($weapon);
         }
         $manager->flush();
     }
 
-    private function getImage($path)
+    private function getImage(array $weaponData)
     {
         $image = new WeaponImage();
-        $image->setName('default weapon image');
-        $image->setPath($path);
+        $image->setName($weaponData[self::IMG_NAME]);
+        $image->setPath($weaponData[self::IMG_PATH]);
         $this->copyRealImage($image);
         return $image;
     }
@@ -57,123 +64,136 @@ class WeaponFixtures extends Fixture
     {
         return [
             [
-                'name'         => 'couteau suisse',
-                'attack'       => 2,
-                'defense'      => 1,
-                'price'        => 6,
-                'uses'         => 5,
-                'lvl'          => 1,
-                self::IMG_PATH => 'test_couteau_suisse.jpg',
+                self::WEAPON_NAME    => 'couteau suisse',
+                self::WEAPON_ATTACK  => 2,
+                self::WEAPON_DEFENSE => 1,
+                self::WEAPON_PRICE   => 6,
+                self::WEAPON_USES    => 5,
+                self::WEAPON_LVL     => 1,
+                self::IMG_PATH       => 'test_couteau_suisse.jpg',
+                self::IMG_NAME       => 'couteau',
             ],
             [
-                'name'         => 'batte',
-                'attack'       => 6,
-                'defense'      => 2,
-                'price'        => 12,
-                'uses'         => 3,
-                'lvl'          => 1,
-                self::IMG_PATH => 'test_batte.jpg',
+                self::WEAPON_NAME    => 'batte',
+                self::WEAPON_ATTACK  => 6,
+                self::WEAPON_DEFENSE => 2,
+                self::WEAPON_PRICE   => 12,
+                self::WEAPON_USES    => 3,
+                self::WEAPON_LVL     => 1,
+                self::IMG_PATH       => 'test_batte.jpg',
+                self::IMG_NAME       => 'batte',
             ],
             [
-                'name'         => 'balais',
-                'attack'       => 5,
-                'defense'      => 5,
-                'price'        => 4,
-                'uses'         => 5,
-                'lvl'          => 1,
-                self::IMG_PATH => 'test_couteau_suisse.jpg',
+                self::WEAPON_NAME    => 'balais',
+                self::WEAPON_ATTACK  => 5,
+                self::WEAPON_DEFENSE => 5,
+                self::WEAPON_PRICE   => 4,
+                self::WEAPON_USES    => 5,
+                self::WEAPON_LVL     => 1,
+                self::IMG_PATH       => 'test_couteau_suisse.jpg',
+                self::IMG_NAME       => 'balais',
             ],
             [
-                'name'         => 'matraque',
-                'attack'       => 2,
-                'defense'      => 1,
-                'price'        => 6,
-                'uses'         => 5,
-                'lvl'          => 1,
-                self::IMG_PATH => 'test_matraque.jpg',
+                self::WEAPON_NAME    => 'matraque',
+                self::WEAPON_ATTACK  => 2,
+                self::WEAPON_DEFENSE => 1,
+                self::WEAPON_PRICE   => 6,
+                self::WEAPON_USES    => 5,
+                self::WEAPON_LVL     => 1,
+                self::IMG_PATH       => 'test_matraque.jpg',
+                self::IMG_NAME       => 'matraque',
             ],
             [
-                'name'         => 'planche',
-                'attack'       => 2,
-                'defense'      => 10,
-                'price'        => 3,
-                'uses'         => 3,
-                'lvl'          => 1,
-                self::IMG_PATH => 'test_planche.jpg',
-            ],
-
-            [
-                'name'         => 'fourche a fumier',
-                'attack'       => 15,
-                'defense'      => 4,
-                'price'        => 12,
-                'uses'         => 7,
-                'lvl'          => 3,
-                self::IMG_PATH => 'test_fourche_fumier.jpg',
+                self::WEAPON_NAME    => 'planche',
+                self::WEAPON_ATTACK  => 2,
+                self::WEAPON_DEFENSE => 10,
+                self::WEAPON_PRICE   => 3,
+                self::WEAPON_USES    => 3,
+                self::WEAPON_LVL     => 1,
+                self::IMG_PATH       => 'test_planche.jpg',
+                self::IMG_NAME       => 'planche de bois',
             ],
 
             [
-                'name'         => 'hache',
-                'attack'       => 20,
-                'defense'      => 1,
-                'price'        => 19,
-                'uses'         => 8,
-                'lvl'          => 5,
-                self::IMG_PATH => 'test_hache.jpg',
+                self::WEAPON_NAME    => 'fourche a fumier',
+                self::WEAPON_ATTACK  => 15,
+                self::WEAPON_DEFENSE => 4,
+                self::WEAPON_PRICE   => 12,
+                self::WEAPON_USES    => 7,
+                self::WEAPON_LVL     => 3,
+                self::IMG_PATH       => 'test_fourche_fumier.jpg',
+                self::IMG_NAME       => 'fourche',
+            ],
+
+            [
+                self::WEAPON_NAME    => 'hache',
+                self::WEAPON_ATTACK  => 20,
+                self::WEAPON_DEFENSE => 1,
+                self::WEAPON_PRICE   => 19,
+                self::WEAPON_USES    => 8,
+                self::WEAPON_LVL     => 5,
+                self::IMG_PATH       => 'test_hache.jpg',
+                self::IMG_NAME       => 'hache',
             ],
             [
-                'name'         => '357 Magnum',
-                'attack'       => 35,
-                'defense'      => 1,
-                'price'        => 60,
-                'uses'         => 7,
-                'lvl'          => 12,
-                self::IMG_PATH => 'test_357.jpg',
+                self::WEAPON_NAME    => '357 Magnum',
+                self::WEAPON_ATTACK  => 35,
+                self::WEAPON_DEFENSE => 1,
+                self::WEAPON_PRICE   => 60,
+                self::WEAPON_USES    => 7,
+                self::WEAPON_LVL     => 12,
+                self::IMG_PATH       => 'test_357.jpg',
+                self::IMG_NAME       => '357 Magnum',
             ],
             [
-                'name'         => 'Bazooka',
-                'attack'       => 55,
-                'defense'      => 2,
-                'price'        => 120,
-                'uses'         => 1,
-                'lvl'          => 15,
-                self::IMG_PATH => 'test_bazook.jpg',
+                self::WEAPON_NAME    => 'Bazooka',
+                self::WEAPON_ATTACK  => 55,
+                self::WEAPON_DEFENSE => 2,
+                self::WEAPON_PRICE   => 120,
+                self::WEAPON_USES    => 1,
+                self::WEAPON_LVL     => 15,
+                self::IMG_PATH       => 'test_bazook.jpg',
+                self::IMG_NAME       => 'Bazooka',
             ],
             [
-                'name'         => 'grenade',
-                'attack'       => 25,
-                'defense'      => 15,
-                'price'        => 60,
-                'uses'         => 1,
-                'lvl'          => 7,
-                self::IMG_PATH => 'test_grenade.jpg',
+                self::WEAPON_NAME    => 'grenade',
+                self::WEAPON_ATTACK  => 25,
+                self::WEAPON_DEFENSE => 15,
+                self::WEAPON_PRICE   => 60,
+                self::WEAPON_USES    => 1,
+                self::WEAPON_LVL     => 7,
+                self::IMG_PATH       => 'test_grenade.jpg',
+                self::IMG_NAME       => 'grenade',
             ],
             [
-                'name'         => 'bouclier',
-                'attack'       => 1,
-                'defense'      => 18,
-                'price'        => 15,
-                'uses'         => 5,
-                'lvl'          => 2,
-                self::IMG_PATH => 'test_bouclier.jpg',
+                self::WEAPON_NAME    => 'bouclier',
+                self::WEAPON_ATTACK  => 1,
+                self::WEAPON_DEFENSE => 18,
+                self::WEAPON_PRICE   => 15,
+                self::WEAPON_USES    => 5,
+                self::WEAPON_LVL     => 2,
+                self::IMG_PATH       => 'test_bouclier.jpg',
+                self::IMG_NAME       => 'bouclier',
             ],
             [
-                'name'         => 'sac de sable',
-                'attack'       => 4,
-                'defense'      => 22,
-                'price'        => 6,
-                'uses'         => 2,
-                'lvl'          => 2,
-                self::IMG_PATH => 'test_sac_sable.jpg',
+                self::WEAPON_NAME    => 'sac de sable',
+                self::WEAPON_ATTACK  => 4,
+                self::WEAPON_DEFENSE => 22,
+                self::WEAPON_PRICE   => 6,
+                self::WEAPON_USES    => 2,
+                self::WEAPON_LVL     => 2,
+                self::IMG_PATH       => 'test_sac_sable.jpg',
+                self::IMG_NAME       => 'sac de sable',
             ],
             [
-                'name'         => 'poing américain',
-                'attack'       => 11,
-                'defense'      => 4,
-                'price'        => 24,
-                'uses'         => 5,
-                'lvl'          => 2,
-                self::IMG_PATH => 'test_poing.jpg',
+                self::WEAPON_NAME    => 'poing américain',
+                self::WEAPON_ATTACK  => 11,
+                self::WEAPON_DEFENSE => 4,
+                self::WEAPON_PRICE   => 24,
+                self::WEAPON_USES    => 5,
+                self::WEAPON_LVL     => 2,
+                self::IMG_PATH       => 'test_poing.jpg',
+                self::IMG_NAME       => 'poing américain',
             ],
         ];
     }
