a753543e1db6f4589f0e44436514a30a7ff7f741
Robin Parisot <rparisot@nvl.fr>
2018-04-10 12:11:44 +0200

    nothing

--- src/AppBundle/Repository/Action/PurchaseRepository.php
+++ src/AppBundle/Repository/Action/PurchaseRepository.php
@@ -13,7 +13,7 @@ class PurchaseRepository extends \Doctrine\ORM\EntityRepository
     public function getPurchases()
     {
         $qb = $this->createQueryBuilder('p')
-                   ->addSelect('p,u,w')
+                   ->addSelect('u,w')
                    ->leftJoin('p.user', 'u')
                    ->leftJoin('p.weapon', 'w')
                    ->orderBy('p.id', 'DESC')
@@ -25,9 +25,8 @@ class PurchaseRepository extends \Doctrine\ORM\EntityRepository
     public function getCountPurchasesByWeapon()
     {
         $qb = $this->createQueryBuilder('p')
-                   ->addSelect('p,u,w')
-                   ->select('COUNT(1) as nb_achat,w.id')
-                   ->leftJoin('p.user', 'u')
+                   ->addSelect('w')
+                   ->select('COUNT(w.id) as nb_achat,w.id')
                    ->leftJoin('p.weapon', 'w')
                    ->groupBy('w.id')
                    ->getQuery()
