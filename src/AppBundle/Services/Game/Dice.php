8084c5e83396dd2c844c4e7f8bf1025458d5903e
Arnaud Kadur <akadur@nvl.fr>
2018-04-12 17:24:07 +0200

    modif robin's dice game

--- src/AppBundle/Services/Game/Dice.php
+++ src/AppBundle/Services/Game/Dice.php
@@ -12,11 +12,21 @@ use UserBundle\Entity\User;
 
 class Dice extends GameMaster
 {
-    const GAME_NAME = 'Dice';
-    const PA_COST   = 1;
-    const NB_DICES  = 5;
+    const GAME_NAME         = 'Dice';
+    const PA_COST           = 1;
+    const NB_DICES          = 5;
+    const INDEX_FACE_NUMBER = 'faceNumber';
+    const INDEX_NB          = 'nb';
 
-    private $dicesRegrouped;
+    private $pair = [];
+
+    private $brelan;
+
+    private $square;
+
+    private $quint;
+
+    private $full = false;
 
     public function execute(User $user, $amount, $data = null)
     {
@@ -26,20 +36,107 @@ class Dice extends GameMaster
         $user->removeMoney($amount);
         $user->removeActionPoint($this->getCost());
 
-        $this->dicesRegrouped = array_count_values($this->launchDices());
+        $dices = array_count_values($this->launcheDices());
 
-        if (!$valeurGagnante = $this->getPair()) {
-            $this->session->getFlashBag()->add('warning', sprintf("Vous avez parié %d$ à %s, mais vous avez perdu. Try again", $amount, $this->getName()));
-            $this->em->persist($user);
-            $this->recordGameAction($user, false, $amount, false);
-            return false;
+        $results = [];
+        foreach ($dices as $faceNumber => $nb) {
+            if (1 == $nb) {
+                continue;
+            }
+            $results[] = $this->hydratResults($faceNumber, $nb);
         }
 
-        $gain = $amount * $valeurGagnante;
-        $this->session->getFlashBag()->add('success', sprintf("Vous avez parié %d$, vous avez fait une paire de %d , vous avez donc gagné %d$", $amount, $valeurGagnante, $gain));
-        $user->addMoney($gain);
-        $this->recordGameAction($user, $gain, $amount, true);
-        $this->em->persist($user);
+        if (!count($results)) {
+            echo "perdu !";
+            die;
+        }
+
+        foreach ($results as $result) {
+            $this->dispatchResults($result[self::INDEX_NB], $result[self::INDEX_FACE_NUMBER]);
+        }
+
+        if ($this->brelan && count($this->pair)) {
+            $this->full = true;
+        }
+
+        if ($this->full) {
+            $this->fullresult();
+        }
+
+        if ($this->quint) {
+            $this->quintResult();
+        }
+
+        if ($this->square) {
+            $this->squareResult();
+        }
+        if ($this->brelan) {
+            $this->brelanResult();
+        }
+
+        if (is_array($this->pair) && !empty($this->pair)) {
+            $this->pairResult();
+        }
+        die;
+    }
+
+    private function fullresult()
+    {
+        echo sprintf("Amazing ! Vous faites un putain de FULL au %d par les %d !", $this->brelan, $this->pair[0]);
+        $this->brelan = null;
+        $this->pair   = null;
+    }
+
+    private function quintResult()
+    {
+        echo sprintf("Ouuahhh ! Vous faites une quinte de %d !", $this->quint);
+    }
+
+    private function squareResult()
+    {
+        echo sprintf("Yooo mannn ! Vous faites un carré de %d !", $this->square);
+    }
+
+    private function brelanResult()
+    {
+        echo sprintf("Youpii mannn ! Vous faites un brelan de %d !", $this->brelan);
+    }
+
+    private function pairResult()
+    {
+        $pairMessage = [];
+
+        foreach ($this->pair as $pair) {
+            $pairMessage[] = sprintf("une paire de %d", $pair);
+        }
+        echo "Gagné ! Vous faites " . implode(' et ', $pairMessage) . ' !';
+    }
+
+    private function dispatchResults($nb, $faceNumber)
+    {
+        switch ($nb) {
+            case 2:
+                $this->pair[] = $faceNumber;
+                break;
+            case 3:
+                $this->brelan = $faceNumber;
+                break;
+            case 4:
+                $this->square = $faceNumber;
+                break;
+            case 5:
+                $this->quint = $faceNumber;
+                break;
+        }
+    }
+
+    private function launcheDices()
+    {
+        $dices = [];
+        for ($i = 0; $i < self::NB_DICES; $i++) {
+            $dices[] = mt_rand(1, 4);
+        }
+        return $dices;
     }
 
     public function getCost()
@@ -52,24 +149,11 @@ class Dice extends GameMaster
         return self::GAME_NAME;
     }
 
-    /**
-     * Return la valeur de la paire si non false
-     * @return false|int|string
-     */
-    private function getPair()
+    private function hydratResults($faceNumber, $nb)
     {
-        return array_search(2, $this->dicesRegrouped);
-    }
-
-    /**
-     * Lances les dés
-     */
-    private function launchDices()
-    {
-        $dices = [];
-        for ($i = 0; $i < self::NB_DICES; $i++) {
-            $dices[] = mt_rand(1, 6);
-        }
-        return $dices;
+        return [
+            self::INDEX_FACE_NUMBER => $faceNumber,
+            self::INDEX_NB          => $nb,
+        ];
     }
 }
---
+++ src/AppBundle/Services/Game/_Dice.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Services\Game;
+
+use UserBundle\Entity\User;
+
+class _Dice extends GameMaster
+{
+    const GAME_NAME = 'Dice';
+    const PA_COST   = 1;
+    const NB_DICES  = 5;
+
+    private $dicesRegrouped;
+
+    public function execute(User $user, $amount, $data = null)
+    {
+        if (!$this->userCanPlay($user, $amount)) {
+            return false;
+        }
+        $user->removeMoney($amount);
+        $user->removeActionPoint($this->getCost());
+
+        $this->dicesRegrouped = array_count_values($this->launchDices());
+
+        if (!$valeurGagnante = $this->getPair()) {
+            $this->session->getFlashBag()->add('warning', sprintf("Vous avez parié %d$ à %s, mais vous avez perdu. Try again", $amount, $this->getName()));
+            $this->em->persist($user);
+            $this->recordGameAction($user, false, $amount, false);
+            return false;
+        }
+
+        $gain = $amount * $valeurGagnante;
+        $this->session->getFlashBag()->add('success', sprintf("Vous avez parié %d$, vous avez fait une paire de %d , vous avez donc gagné %d$", $amount, $valeurGagnante, $gain));
+        $user->addMoney($gain);
+        $this->recordGameAction($user, $gain, $amount, true);
+        $this->em->persist($user);
+    }
+
+    public function getCost()
+    {
+        return self::PA_COST;
+    }
+
+    public function getName()
+    {
+        return self::GAME_NAME;
+    }
+
+    /**
+     * Return la valeur de la paire si non false
+     * @return false|int|string
+     */
+    private function getPair()
+    {
+        return array_search(2, $this->dicesRegrouped);
+    }
+
+    /**
+     * Lances les dés
+     */
+    private function launchDices()
+    {
+        $dices = [];
+        for ($i = 0; $i < self::NB_DICES; $i++) {
+            $dices[] = mt_rand(1, 6);
+        }
+        return $dices;
+    }
+}
