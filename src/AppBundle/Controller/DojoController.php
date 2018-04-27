f7332d2320af28703094f314d9d44bc708479104
Robin <rparisot@nvl.fr>
2018-04-27 17:52:43 +0200

    WIP dojo competences

---
+++ src/AppBundle/Controller/DojoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Services\Dojo\ManageCompetences;
+use AppBundle\Utils\Dojo\Helper;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DojoController extends Controller
+{
+    public function dojoIndexAction()
+    {
+        $priceAttack  = Helper::getAttackPrice($this->getUser());
+        $priceDefense = Helper::getDefensePrice($this->getUser());
+        $priceSkill   = Helper::getSkillPrice($this->getUser());
+        return $this->render(':dojo:dojo.html.twig', [
+            'priceAttack'  => $priceAttack,
+            'priceDefense' => $priceDefense,
+            'priceSkill'   => $priceSkill,
+        ]);
+    }
+
+    public function dojoAddAttackAction()
+    {
+        ManageCompetences::addAttackToUser($this->getUser());
+        return $this->redirectToRoute('dojo_index');
+    }
+
+    public function dojoAddDefenseAction()
+    {
+        ManageCompetences::addDefenseToUser($this->getUser());
+        return $this->redirectToRoute('dojo_index');
+    }
+
+    public function dojoAddSkillAction()
+    {
+        ManageCompetences::addSkillToUser($this->getUser());
+        return $this->redirectToRoute('dojo_index');
+    }
+}
